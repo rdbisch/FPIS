395f0c7e7826c7692eae8007e09a8a0a2979a03f
Rick Bischoff <rdbisch@gmail.com>
2015-07-12 15:04:26 -0500

    Added factorial

--- ch2/MyModule.scala
+++ ch2/MyModule.scala
@@ -3,17 +3,30 @@
 /** A documentation comment */
 
 object MyModule {
-  def abs(n: Int): Int = 
+  def abs(n: Int): Int =
     if (n < 0) -n
     else n
+
+  def factorial(n: Int): Int = {
+    @annotation.tailrec
+    def go(n: Int, acc: Int): Int =
+      if (n <= 0) acc
+      else go(n - 1, n*acc)
+
+    go(n, 1)
+  }
 
   private def formatAbs(x: Int) = {
     val msg = "The absolute value of %d is %d."
     msg.format(x, abs(x))
   }
 
+  private def formatFactorial(n: Int) = {
+    val msg ="The factorial of %d is %d."
+    msg.format(n, factorial(n))
+  }
+
   def main(args: Array[String]): Unit =
     println(formatAbs(-42))
-  
+    println(formatFactorial(7))
 }
-
