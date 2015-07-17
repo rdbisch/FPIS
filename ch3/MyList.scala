d1e51ec06d187f8fa2bd98569bc9bb6953e4d808
Rick Bischoff <rdbisch@gmail.com>
2015-07-16 19:33:01 -0500

    Added some exercises

--- ch3/MyList.scala
+++ ch3/MyList.scala
@@ -8,4 +8,25 @@ object MyList {
   def apply[A](as: A*): MyList[A] = 
     if (as.isEmpty) Nil
     else Cons(as.head, apply(as.tail: _*))
+
+  /* Exercise 3.2 */
+  def tail[A](el: MyList[A]): MyList[A] = el match {
+    case Nil => Nil
+    case Cons(x, xs) => xs
+  }
+
+  /* Exercise 3.3 */
+  def setHead[A](el: MyList[A], a: A): MyList[A] = el match {
+    case Nil => Nil
+    case Cons(x, xs) => Cons(a, xs)
+  }
+
+  /* Exercise 3.4 */
+  def drop[A](el: MyList[A], n: Int): MyList[A] = n match {
+    case 0 => el
+    case _ => el match {
+      case Nil => Nil
+      case Cons(x, xs) => drop(xs, n - 1)
+    }
+  }
 }
