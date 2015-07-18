c54262a0d95136635ca5d6259f90a6936f5d02a8
Rick Bischoff <rdbisch@gmail.com>
2015-07-17 20:21:40 -0500

    partial completion fo exercise

--- ch4/MyOption.scala
+++ ch4/MyOption.scala
@@ -1,12 +1,23 @@
-sealed trait MyOption[+A]
+trait MyOption[+A] {
+  def map[B](f: A => B): MyOption[B] = this match {
+    case None    => None
+    case Some(a) => Some(f(a))
+  }
+
+  def getOrElse[B >: A](default : => B): B = this match {
+    case None => default
+    case Some(a) => a
+  }
+
+  def flatMap[B](f: A => MyOption[B]): MyOption[B] =
+    map(f) getOrElse None
+
+  def filter(f: A => Boolean): MyOption[A] = 
+    this match {
+      case Some(a) if f(a) => this
+      case _ => None
+    }
+}
 case class Some[+A](get: A) extends MyOption[A]
 case object None extends MyOption[Nothing]
 
-object MyOption {
-    def map[A,B](opt: MyOption[A])(f: A => B): MyOption[B] = opt match {
-        case None    => None
-        case Some(a) => Some(f(a))
-    }
-
-    def 
-}
